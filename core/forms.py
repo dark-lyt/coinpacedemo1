994b627c673a0ae1b3f9c9c9db1dfbea1f4ff062
michaellyon <michaellyon@localhost.localdomain>
2021-05-05 05:58:29 +0100

    update model

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from django.forms.fields import IntegerField
 
 class PayForm(forms.Form):
     coin_amount = forms.IntegerField()
  
+
+class ContactForm(forms.Form):
+    pass
+
+class WithdrawForm(forms.Form):
+    amount = forms.IntegerField()
+    address = forms.Textarea()
--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.db import models
 from django.shortcuts import reverse
 from django_countries.fields import CountryField
+from cryptocurrency_payment.models import CryptoCurrencyPayment
 
 CATEGORY_CHOICES = (
     ('BP', 'Baby Plan'),
@@ -61,33 +62,10 @@ class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    # quantity = models.IntegerField(default=1)
     ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user} on {self.item.title} plan'
-
-    # def get_total_item_price(self):
-    #     return (self.quantity * self.item.price)
-
-    # def get_total_item_discount_price(self):
-    #     return (self.quantity * self.item.discount_price)
-
-    # def get_amount_saved(self):
-    #     return (self.get_total_item_price() - self.get_total_item_discount_price())
-
-    # def get_final_price(self):
-    #     if self.item.discount_price:
-    #         return self.get_total_item_discount_price()
-    #     return self.get_total_item_price()
-
-
-# class Coupon(models.Model):
-#     code = models.CharField(max_length=15)
-#     amount = models.FloatField(max_length=10)
-
-#     def __str__(self):
-#         return self.code
 
 
 class Order(models.Model):
@@ -98,85 +76,16 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
-    # billing_address = models.ForeignKey(
-    #     'Address', related_name="billingpyth_address", on_delete=models.SET_NULL, null=True, blank=True)
-
-    # shipping_address = models.ForeignKey(
-    #     'Address', related_name="shipping_address", on_delete=models.SET_NULL, null=True, blank=True)
-    # payment = models.ForeignKey(
-    #     'Payment', on_delete=models.SET_NULL, null=True, blank=True)
-
-    # coupon = models.ForeignKey(
-    #     'Coupon', on_delete=models.SET_NULL, null=True, blank=True)
-
-    # being_delivered = models.BooleanField(default=False)
-    # recieved = models.BooleanField(default=False)
-    # refund_requested = models.BooleanField(default=False)
-    # refund_granted = models.BooleanField(default=False)
-    '''
-    Things to keep track of.
-        1. Item to cart
-        2. Add billing address
-        (Failed Checkouts)
-        3. Payments
-        (Preprocessing, processing, packaging etc.)
-        4. Being Delivered
-        5. Recieved
-        6. Refunds
-    '''
 
     def __str__(self):
         return self.user.username
 
-    # def get_total(self):
-    #     total = 0
-    #     for order_item in self.items.all():
-    #         total += order_item.get_final_price()
-    #     if self.coupon:
-    #         total -= self.coupon.amount
-    #     return total
 
 
-# class Address(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                              on_delete=models.CASCADE)
-#     street_address = models.CharField(max_length=100)
-#     appartment_address = models.CharField(max_length=100)
-#     country = CountryField(multiple=False)
-#     zip = models.CharField(max_length=100)
-#     address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
-#     default = models.BooleanField(default=False)
+class Withdraw(models.Model):
+    order = models.ForeignKey(CryptoCurrencyPayment, on_delete=models.CASCADE)
+    sent = models.BooleanField(default=False)
+    amount = models.IntegerField()
+    def __str__(self):
+        return f"{self.pk}"
 
-#     def __str__(self):
-#         return self.user.username
-
-#     class Meta:
-#         verbose_name_plural = 'Addresses'
-
-
-# class Payment(models.Model):
-#     stripe_charge_id = models.CharField(max_length=50, null=True, blank=True)
-#     braintreeID = models.CharField(max_length=50, null=True, blank=True)
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                              on_delete=models.SET_NULL, blank=True, null=True)
-#     amount = models.FloatField()
-#     timestamp = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.user.username
-
-
-# class Refund(models.Model):
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-#     reason = models.TextField()
-#     accepted = models.BooleanField(default=False)
-#     email = models.EmailField()
-
-#     def __str__(self):
-#         return f"{self.pk}"
-
-# def userprofile_reciever(sender, instance, created, *args, **kwargs):
-#     if created:
-#         userprofile = UserProfile.objects.create(user=instance)
-
-# post_save.connect(userprofile_reciever, sender=settings.AUTH_USER_MODEL)
